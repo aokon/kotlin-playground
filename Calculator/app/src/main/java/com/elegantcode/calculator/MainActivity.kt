2fe0063b648a02baedd374b25e65f33882d112f8
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-05 21:22:38 +0200

    calculator: add basic UI and handlers for basic calculations

--- Calculator/app/src/main/java/com/elegantcode/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/elegantcode/calculator/MainActivity.kt
@@ -2,11 +2,45 @@ package com.elegantcode.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    private fun String.toEditable(): Editable? = Editable.Factory.getInstance().newEditable(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        additionBtn.setOnClickListener {
+            val  result = operandOne.text.toString().toDouble() + operandTwo.text.toString().toDouble()
+            resultText.text = result.toString()
+        }
+
+        substractionBtn.setOnClickListener {
+            val  result = operandOne.text.toString().toDouble() - operandTwo.text.toString().toDouble()
+            resultText.text = result.toString()
+        }
+
+        multiplicationBtn.setOnClickListener {
+            val  result = operandOne.text.toString().toDouble() * operandTwo.text.toString().toDouble()
+            resultText.text = result.toString()
+        }
+
+        divisionBtn.setOnClickListener {
+            val  result = operandOne.text.toString().toDouble() / operandTwo.text.toString().toDouble()
+            resultText.text = result.toString()
+        }
+
+        clearBtn.setOnClickListener {
+            operandOne.text = "".toEditable()
+            operandTwo.text = "".toEditable()
+            resultText.text = "0.00"
+            operandOne.requestFocus()
+            Toast.makeText(applicationContext, "Operands values were deleted. Let's try again", Toast.LENGTH_LONG).show()
+        }
     }
 }
+
