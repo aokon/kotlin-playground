c3f2c53e164630d22c5197c0e466a259f2fdc0aa
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-09 16:29:37 +0200

    ReTodo: add loading data to the list via ViewModel

--- ReTodo/app/src/main/java/pl/rebased/retodo/MainActivity.kt
+++ ReTodo/app/src/main/java/pl/rebased/retodo/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import androidx.lifecycle.*
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
@@ -13,22 +15,30 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_todo.view.*
 
-data class Todo(val name: String, val isCompleted: Boolean)
+interface ChangeTodoHandler {
+  fun onDoneChange(item: Todo, done: Boolean)
+  fun onDescriptionChange(item: Todo, text: String)
+}
 
-class MainActivity : AppCompatActivity() {
+data class Todo(var name: String, var isCompleted: Boolean)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val todoAdapter = TodoAdapter()
+typealias TodoList =  List<Todo>
 
-        todoList.adapter = todoAdapter
-        todoAdapter.submitList(makeTodoList())
-        todoList.layoutManager = LinearLayoutManager(this as Context, LinearLayoutManager.VERTICAL, false)
+class TodoViewModel : ViewModel() {
+    val todos : LiveData<TodoList> by lazy {
+        MutableLiveData(todoList())
     }
 
-    private fun makeTodoList(): List<Todo> {
-        return listOf<Todo>(
+    fun setDone(todo: Todo, done: Boolean) {
+        todo.isCompleted = done
+    }
+
+    fun setDescription(todo: Todo, text: String) {
+        todo.name = text
+    }
+
+    private fun todoList(): List<Todo> {
+        return listOf(
             Todo("Todo 1", true),
             Todo("Todo 2", true),
             Todo("Todo 3", false),
@@ -38,7 +48,7 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class TodoAdapter: ListAdapter<Todo, TodoViewHolder>(diff) {
+class TodoAdapter(private val handler: ChangeTodoHandler) : ListAdapter<Todo, TodoViewHolder>(diff) {
     companion object diff: DiffUtil.ItemCallback<Todo>() {
         override fun areItemsTheSame(oldItem: Todo, newItem: Todo): Boolean = oldItem == newItem
 
@@ -51,13 +61,53 @@ class TodoAdapter: ListAdapter<Todo, TodoViewHolder>(diff) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
-        return TodoViewHolder(itemView)
+        return TodoViewHolder(itemView, handler)
     }
 }
 
-class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class TodoViewHolder(itemView: View, val handler: ChangeTodoHandler) : RecyclerView.ViewHolder(itemView) {
     fun bindTo(item: Todo) {
+        itemView.checkBox.setOnCheckedChangeListener { view, isChecked ->
+            handler.onDoneChange(item, isChecked)
+        }
+        itemView.textView.setOnEditorActionListener { v, actionId, event ->
+                when (actionId) {
+                    EditorInfo.IME_ACTION_DONE, EditorInfo.IME_ACTION_NEXT, EditorInfo.IME_NULL -> {
+                        handler.onDescriptionChange(item, v.text.toString())
+                        true
+                    }
+                    else -> false
+                }
+        }
         itemView.checkBox.isChecked = item.isCompleted
-        itemView.textView.text = item.name
+        itemView.textView.setText(item.name)
+    }
+}
+
+class MainActivity : AppCompatActivity(), ChangeTodoHandler {
+    // we want to avoid moving  model to GC when
+    val model by lazy {
+        ViewModelProvider(this).get(TodoViewModel::class.java)
+    }
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val todoAdapter = TodoAdapter(this)
+
+        todoList.adapter = todoAdapter
+        todoList.layoutManager = LinearLayoutManager(this as Context, LinearLayoutManager.VERTICAL, false)
+
+        model.todos.observe(this, Observer {
+           todoAdapter.submitList(it)
+        })
+    }
+
+    override fun onDoneChange(item: Todo, done: Boolean) {
+        model.setDone(item, done)
+    }
+
+    override fun onDescriptionChange(item: Todo, text: String) {
+        model.setDescription(item, text)
     }
 }
