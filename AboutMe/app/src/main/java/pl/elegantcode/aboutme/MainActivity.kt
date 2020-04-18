afb01e1acd5580ce86bda87c6828f0479c361c7d
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-18 21:43:03 +0200

    About me: apply nickname change

--- AboutMe/app/src/main/java/pl/elegantcode/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/pl/elegantcode/aboutme/MainActivity.kt
@@ -2,11 +2,20 @@ package pl.elegantcode.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        done_button.setOnClickListener { button: View ->
+            nickname_text.text = name_edit.text
+            nickname_text.visibility = View.VISIBLE
+            name_edit.visibility = View.GONE
+            button.visibility = View.GONE
+        }
     }
 }
