348fe5b2af8513be2cb3365b583aa64fad0a8323
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-19 11:38:02 +0200

    AboutMe: hide the keyboard after done btn was clicked

--- AboutMe/app/src/main/java/pl/elegantcode/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/pl/elegantcode/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package pl.elegantcode.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,10 @@ class MainActivity : AppCompatActivity() {
             nickname_text.visibility = View.VISIBLE
             name_edit.visibility = View.GONE
             button.visibility = View.GONE
+
+            // hide the keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(button.windowToken, 0)
         }
     }
 }
