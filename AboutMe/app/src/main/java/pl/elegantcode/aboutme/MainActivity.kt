d26208f97176348360b16ed4c68fbdf6d74c748f
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-19 11:53:12 +0200

    AboutMe: add handling toggle between nickname_text and name_edit

--- AboutMe/app/src/main/java/pl/elegantcode/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/pl/elegantcode/aboutme/MainActivity.kt
@@ -13,15 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        done_button.setOnClickListener { button: View ->
+        done_button.setOnClickListener {
             nickname_text.text = name_edit.text
             nickname_text.visibility = View.VISIBLE
             name_edit.visibility = View.GONE
-            button.visibility = View.GONE
+            it.visibility = View.GONE
 
             // hide the keyboard
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(button.windowToken, 0)
+            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        }
+
+        nickname_text.setOnClickListener {
+            it.visibility = View.GONE
+            name_edit.visibility = View.VISIBLE
+            name_edit.requestFocus()
+            done_button.visibility = View.VISIBLE
+
+            // show the keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.showSoftInput(name_edit, 0)
         }
     }
 }
