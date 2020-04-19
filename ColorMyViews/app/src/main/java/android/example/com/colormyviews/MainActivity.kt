86a3be0a25627b3de82e67f9b77147d991ca3b5c
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-19 20:57:37 +0200

    ColorMyViews: add handling the onClick for box views

--- ColorMyViews/app/src/main/java/android/example/com/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/android/example/com/colormyviews/MainActivity.kt
@@ -1,12 +1,40 @@
 package android.example.com.colormyviews
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val boxesList: List<View> = listOf(
+            box_one_text,
+            box_two_text,
+            box_three_text,
+            box_four_text,
+            box_five_text
+        )
+
+        for (box in boxesList) {
+            box.setOnClickListener { handleOnClick(it) }
+        }
+    }
+
+    private fun handleOnClick(view: View) = when (view.id) {
+        R.id.box_one_text -> view.setBackgroundColor(Color.CYAN)
+        R.id.box_two_text -> view.setBackgroundColor(Color.MAGENTA)
+        R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+        R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_red_light)
+        R.id.box_five_text -> view.setBackgroundResource(android.R.color.background_dark)
+        else -> view.setBackgroundColor(Color.LTGRAY)
     }
 }
