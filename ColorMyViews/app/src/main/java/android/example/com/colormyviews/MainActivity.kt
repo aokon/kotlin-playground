99ffe214b7853f3da6d05d4ba8dc4d5467d580ea
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-20 15:33:58 +0200

    ColorMyViews: add handler for bottom buttons

--- ColorMyViews/app/src/main/java/android/example/com/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/android/example/com/colormyviews/MainActivity.kt
@@ -19,7 +19,10 @@ class MainActivity : AppCompatActivity() {
             boxTwoText,
             boxThreeText,
             boxFourText,
-            boxFiveText
+            boxFiveText,
+            redButton,
+            yellowButton,
+            greenButton
         )
     }
 
@@ -33,6 +36,12 @@ class MainActivity : AppCompatActivity() {
         R.id.boxThreeText -> view.setBackgroundResource(android.R.color.holo_blue_bright)
         R.id.boxFourText -> view.setBackgroundResource(android.R.color.holo_red_light)
         R.id.boxFiveText -> view.setBackgroundResource(android.R.color.background_dark)
+
+        // bottom buttons
+        R.id.redButton -> boxThreeText.setBackgroundResource(R.color.red)
+        R.id.yellowButton -> boxFiveText.setBackgroundResource(R.color.yellow)
+        R.id.greenButton -> boxOneText.setBackgroundResource(R.color.green)
+
         else -> view.setBackgroundColor(Color.LTGRAY)
     }
 }
