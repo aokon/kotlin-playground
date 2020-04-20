8e59b13da1e09ac03514e335cacae75215be0955
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-20 17:48:06 +0200

    adroid-trivia: add navigation between title and game fragments

--- android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,13 +1,19 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
+import kotlinx.android.synthetic.main.fragment_title.*
+import java.util.logging.Logger
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +25,12 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//             This is not working...
+//            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        }
 
         return binding.root
     }
