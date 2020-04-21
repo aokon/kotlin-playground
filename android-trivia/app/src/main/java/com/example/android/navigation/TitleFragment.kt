7a0c1ef9f9a762348a9e277676caee6ad9c2dd04
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-21 17:22:53 +0200

    android-trivia: add setup for menu in the action bar for TitleFragment

--- android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ android-trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,15 +2,14 @@ package com.example.android.navigation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 import kotlinx.android.synthetic.main.fragment_title.*
 import java.util.logging.Logger
@@ -32,7 +31,19 @@ class TitleFragment : Fragment() {
 //            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         }
 
+        setHasOptionsMenu(true) // Enable menu in the action bar
+
         return binding.root
     }
 
+    // Apply the menu resource for menu in the action bar
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+               || super.onOptionsItemSelected(item)
+    }
 }
