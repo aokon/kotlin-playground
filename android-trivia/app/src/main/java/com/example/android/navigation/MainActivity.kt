55eeeec9b22864d480f4b2ba6b95e6ca7300768d
Adam Okoń <adam.okon@elegantcode.pl>
2020-04-23 17:10:58 +0200

    android-trivia: display drawer menu only for startDestination

--- android-trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ android-trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -43,6 +43,13 @@ class MainActivity : AppCompatActivity() {
         // Setup Navigation in the ActionBar
         drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.navHostFragment)
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _ ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
